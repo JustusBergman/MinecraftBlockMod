8dda32fc4120d091ca254097382c9454b664a026
JustusBergman <mr.lollax@gmail.com>
2022-08-04 15:44:17 +0200

    Items and item groups work now.

--- src/main/java/net/justus/firstmod/item/ModItems.java
+++ src/main/java/net/justus/firstmod/item/ModItems.java
@@ -3,13 +3,14 @@ package net.justus.firstmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.justus.firstmod.FirstMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item MIN_STEN = registerItem("min_sten",
+            new Item(new FabricItemSettings().group(ModItemGroup.STENAR)));
+    public static final Item MIN_ANDRA_STEN = registerItem("min_andra_sten",
             new Item(new FabricItemSettings().group(ModItemGroup.STENAR)));
 
     private static Item registerItem(String name, Item item) {
